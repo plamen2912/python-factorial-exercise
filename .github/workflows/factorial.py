c1345e7e0aeb21ebe60233efd529b29ea359a687
plamen2912 <153605431+plamen2912@users.noreply.github.com>
2023-12-12 12:35:54 +0200

    Create factorial.py
    Initial Python code to calculate the factorial of a number. Here's a simple example:

---
+++ .github/workflows/factorial.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+number = int(input("Enter a number to find its factorial: "))
+print(f"The factorial of {number} is {factorial(number)}.")
